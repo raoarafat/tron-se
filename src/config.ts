3b50b2202f90e520c74e4d06686abc1fa8d76afb
arafat <arafat@mail.com>
2025-05-25 23:56:27 +0800

    in

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  network: 'shasta',
-  privateKey:
-    '12b7f64b2d29667689dbe0daaded944f97a6c6f63c8036e46eefcddedfbbe511', // Add your private key here if needed
-  fullNode: 'https://api.shasta.trongrid.io',
-  solidityNode: 'https://api.shasta.trongrid.io',
-  eventServer: 'https://api.shasta.trongrid.io',
-  apiKey: '1faccd87-931c-449e-832a-819b988df90d', // Add your TronGrid API key here if needed
-};
